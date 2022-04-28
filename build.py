eac61b94088353ac38c50df5b8da85343390d296
phiarchitect <phi@phiarchitect.com>
2022-04-28 06:03:43 -0700

    add build sript for sessions

---
+++ build.py
@@ -0,0 +1,26 @@
+from sessions import *
+
+
+def main():
+    MUSIC = True
+    TICK = True
+    root = pm.N.E3
+    octaves = 3
+    scale_type = pm.S.pentatonic_major
+
+    scale = pm.build_scale(
+        root=root, 
+        scale_type=scale_type, 
+        octaves=octaves)
+
+    bpm = 120
+    tempo = int(pm.bpm2tempo(bpm))
+
+    #  build_session('.', scale, tempo, tick=TICK, music=MUSIC)
+    build_sequence('.', scale, tempo, tick=TICK, music=MUSIC)
+
+
+if __name__ == '__main__':
+    main()
+
+
